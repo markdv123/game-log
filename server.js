17a102bd1eaf601b35d408b146f4c93117135ad6
Mark DeVito <markdv@alumni.unc.edu>
2020-11-11 10:52:42 -0500

    copied middleware from template and created env

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const helmet = require('helmet')
 const connection = require('./db/connection.js')
 
 const PORT = process.env.PORT || 3001
 const app = express()
 
 app.use(logger('dev'))
+app.use(helmet())
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.disable('X-Powered-By')
 app.get('/', (req, res) => res.send({ msg: 'Server Working' }))
 app.use('/api', AppRouter)
 
