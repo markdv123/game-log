700242b254585d6900f09ec17b18bd42f7bb4aa0
Mark DeVito <markdv@alumni.unc.edu>
2020-11-13 10:19:02 -0500

    added auth stuff to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,62 @@ import './styles/App.css';
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      authenticated: false,
+      currentUser: null,
+      pageLoading: true
+    }
+  }
+
+  verifyTokenValid = async () => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      try {
+        const session = await __CheckSession()
+        this.setState(
+          {
+            currentUser: session,
+            authenticated: true
+          },
+          () => this.props.history.push('/profile')
+        )
+      } catch (error) {
+        this.setState({ currentUser: null, authenticated: false })
+        localStorage.clear()
+      }
+    }
+  }
+
+  toggleAuthenticated = (value, user, done) => {
+    this.setState({ authenticated: value, currentUser: user }, () => done())
+  }
+
+  componentDidMount() {
+    this.verifyTokenValid()
+    this.setState({ pageLoading: false })
+  }
+
+  verifyTokenValid = async () => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      try {
+        const session = await __CheckSession()
+        console.log('session', session)
+        this.setState(
+          {
+            currentUser: session.user,
+            authenticated: true
+          },
+          () => this.props.history.push('/profile')
+        )
+      } catch (error) {
+        this.setState({ currentUser: null, authenticated: false })
+        localStorage.clear()
+      }
+    }
+  }
   
    render() {
        return (
