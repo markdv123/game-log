262349a66fbbf32e635b6b65e2f77107d2802afd
Mark DeVito <markdv@alumni.unc.edu>
2020-11-15 11:41:58 -0500

    posts appear on game page

--- client/src/pages/GamePage.js
+++ client/src/pages/GamePage.js
@@ -7,8 +7,10 @@
 //import another component that displays all post about the game by querying the posts by game name from my api
 
 import Axios from 'axios'
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
+import { __GetPostsByGame } from '../services/PostServices'
+import Post from '../components/Post'
 
 class GamePage extends Component {
     constructor(props){
@@ -16,16 +18,28 @@ class GamePage extends Component {
         this.state = {
             gameId: props.match.params.game_id,
             gameDetails: {},
+            posts: []
         }
     }
 
     componentDidMount() {
         this.getGameDetails()
+        this.getPosts()
     }
     
     getGameDetails = async () => {
         const res = await Axios.get(`https://api.rawg.io/api/games/${this.state.gameId}`)
         this.setState({gameDetails: res.data})
+    }
+
+    getPosts = async () => {
+        try {
+            const response = await __GetPostsByGame(this.state.gameId)
+            this.setState({posts: response.posts})
+            console.log(this.state)
+        } catch (err) {
+            throw err
+        }
     }
 
     render() {
@@ -38,6 +52,18 @@ class GamePage extends Component {
                     onClick={() => {
                         this.props.history.push(`/upload/${gameDetails.id}`)
                     }}><i class="material-icons left">add</i>Create Post</a>
+                {this.state.posts.map((post) => (
+                        <Post
+                            onClick={() =>
+                                this.props.history.push(`/posts/${post.id}`)
+                            }
+                            key={post.id}
+                            title={post.title}
+                            rating={post.rating}
+                            image={post.image_url}
+                            username={post.username}
+                        />
+                    ))}
             </div>
 
         )
