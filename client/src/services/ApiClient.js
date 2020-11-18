165f426543ea812e5b26e1acff95061f5f556f19
Mark DeVito <markdv@alumni.unc.edu>
2020-11-17 20:26:57 -0500

    deploying again

--- client/src/services/ApiClient.js
+++ client/src/services/ApiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const ApiClient = axios.create({ baseURL: 'http://localhost:3001/api' })
+const ApiClient = axios.create({ baseURL: process.env.NODE_ENV === 'production' ? `${window.location.origin}/api` : 'http://localhost:3001/api' })
 
 ApiClient.interceptors.request.use(
   async (config) => {
