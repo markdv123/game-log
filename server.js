a8aaa26e80318e8747d3bc95de39c945ddb215de
Mark DeVito <markdv@alumni.unc.edu>
2020-11-19 14:10:26 -0500

    please deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const AppRouter = require('./routes/AppRouter')
+const AppRouter = require('./Routes/AppRouter')
 const logger = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
